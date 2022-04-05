472a637751b85fb077844a63a436b7a1275e7e4d
Vasilii <plambir93kek55@gmail.com>
2022-04-05 15:59:53 +0600

    #2: add empty management app

---
+++ apps/management/src/app/linking.ts
@@ -0,0 +1,26 @@
+import { getStateFromPath, PathConfig } from '@react-navigation/native';
+import { LinkingOptions } from '@react-navigation/native/lib/typescript/src/types';
+import * as Linking from 'expo-linking';
+
+type ScreenLinkingOptions = Parameters<typeof getStateFromPath>[1];
+type ScreenRoutingOptions<T extends object> = {
+  [key in keyof T]?: string | ScreenLinkingOptions | PathConfig<object>;
+};
+
+const appLink = Linking.makeUrl('/');
+
+export const appLinking: LinkingOptions<object> = {
+  prefixes: [appLink],
+  config: {
+    screens: {
+      Public: {
+        initialRouteName: '',
+        screens: {}
+      },
+      Private: {
+        initialRouteName: '',
+        screens: {}
+      }
+    }
+  }
+};
---
+++ apps/management/src/app/root.tsx
@@ -0,0 +1,21 @@
+import { appConfig } from './constants';
+import { registerRootComponent } from 'expo';
+import React, { ReactElement } from 'react';
+import * as Sentry from 'sentry-expo';
+import { App } from './app';
+
+export default function Root(): ReactElement {
+  if (appConfig.sentry.enabled) {
+    Sentry.init({
+      dsn: appConfig.sentry.dsn,
+      enableInExpoDevelopment: false,
+      debug: !appConfig.production
+    });
+  }
+
+  return (
+    <App />
+  );
+}
+
+registerRootComponent(Root);
