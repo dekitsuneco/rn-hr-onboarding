e8417a8e289d4cd8f55d559169fb32324aeef2ca
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-04-07 16:07:25 +0500

    #4 Add basic configuration files for styles

---
+++ packages/ui-kit/variables.ts
@@ -0,0 +1,39 @@
+export const variables = {
+  contentOffset: '1.5rem',
+  color: {
+    background: '#FFFFFF',
+    backgroundSecondary: '#F9FAFB',
+    backgroundTertiary: '#FCF5EF',
+    danger: '#E71D32',
+    black: '#000000',
+    green: '#5DCE41',
+    white: '#FFFFFF',
+    gray: '#C4C9CE',
+    primary: '#26a0f8',
+    primaryDark: '#0787E3',
+    primaryDarker: '#0C79C8',
+    textInverse: '#FFFFFF',
+    textPrimary: '#181818',
+    textSecondary: '#555555',
+    textTertiary: '#93989C',
+    textDanger: '#E72005',
+    statusNeutral: '#8294A2',
+    statusSuccess: '#50F826',
+    statusDanger: '#E72005',
+    statusProgress: '#26A0F8'
+  },
+  fontSize: {
+    smallest: '0.85rem',
+    small: '1rem',
+    medium: '1.15rem',
+    larger: '1.4rem',
+    large: '1.55rem',
+    largest: '1.7rem'
+  },
+  fontFamily: {
+    sfProDisplayBold: 'SFProDisplayBold',
+    sfProDisplayRegular: 'SFProDisplayRegular',
+    sfProTextRegular: 'SFProTextRegular',
+    sfProTextSemiBold: 'SFProTextSemiBold'
+  }
+};
