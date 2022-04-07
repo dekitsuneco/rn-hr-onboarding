2f0ffefae02656ee703a859984a23817ae99dfd4
Akira <47333988+dekitsuneco@users.noreply.github.com>
2022-04-07 19:44:13 +0500

    #4 Add fonts to ASSETS and require them in Management & Onboarding

--- apps/onboarding/src/app/root.tsx
+++ apps/onboarding/src/app/root.tsx
@@ -14,7 +14,12 @@ export default function Root(): ReactElement {
     });
   }
 
-  const [areFontsReady] = useFonts({});
+  const [areFontsReady] = useFonts({
+    SFProDisplayBold: require('assets/fonts/SF-Pro-Display-Bold.otf'),
+    SFProDisplayRegular: require('assets/fonts/SF-Pro-Display-Regular.otf'),
+    SFProTextRegular: require('assets/fonts/SF-Pro-Text-Regular.otf'),
+    SFProTextSemiBold: require('assets/fonts/SF-Pro-Text-Semibold.otf')
+  });
 
   if (!areFontsReady) {
     return null;
