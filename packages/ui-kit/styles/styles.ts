d65a0a377b8f16c0ee78c61370043c3933dc3407
dekitsuneco <47333988+dekitsuneco@users.noreply.github.com>
2022-04-12 23:47:40 +0500

    #7 Remove form styles from commonStyles as their unnecessary now as global styles

--- packages/ui-kit/styles/styles.ts
+++ packages/ui-kit/styles/styles.ts
@@ -17,46 +17,6 @@ export const commonStyle = createStyles({
   container: {
     paddingHorizontal: variables.contentOffset
   },
-  formControl: {
-    position: 'relative',
-    width: '100%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft: 20,
-    paddingRight: 20,
-    backgroundColor: variables.color.backgroundSecondary,
-    borderColor: variables.color.backgroundSecondary,
-    borderWidth: 1,
-    borderRadius: 10
-  },
-  formInput: {
-    flex: 1,
-    height: 44,
-    paddingTop: 10,
-    paddingBottom: 10,
-    color: variables.color.textPrimary,
-    fontSize: variables.fontSize.medium
-  },
-  formControlFocus: {
-    backgroundColor: variables.color.background,
-    borderColor: variables.color.primary
-  },
-  formControlError: {
-    backgroundColor: variables.color.backgroundTertiary,
-    borderColor: variables.color.danger
-  },
-  formInputDisabled: {
-    backgroundColor: variables.color.gray + '33',
-    opacity: 0.5
-  },
-  formInputIcon: {
-    position: 'absolute',
-    top: 0,
-    right: 20,
-    justifyContent: 'center',
-    height: '100%',
-    width: 30
-  },
   row: {
     display: 'flex',
     flexDirection: 'row',
