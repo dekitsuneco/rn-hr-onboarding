b38937cdffc43a0d855e09bd3d3119a9c957c11d
dekitsuneco <47333988+dekitsuneco@users.noreply.github.com>
2022-04-12 16:07:20 +0500

    #7 Create InputFormGroup component as wrapper for TextInput

---
+++ packages/ui-kit/input-form-group/component.tsx
@@ -0,0 +1,56 @@
+import { FormGroup } from '../form-group';
+import { InputType } from '../text-input';
+import { AppTextInput } from '../text-input';
+import React, { ReactElement, useState } from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+import { Icons } from 'assets/icons';
+import { Icon } from '../icon';
+
+export interface InputFormGroupProps {
+  label?: string;
+  message?: string;
+  type?: InputType;
+  containerStyle?: StyleProp<ViewStyle>;
+}
+
+export function InputFormGroup({
+  label,
+  message,
+  type,
+  containerStyle,
+  ...restProps
+}: InputFormGroupProps): ReactElement {
+  const [hasError, setError] = useState(false);
+
+  const typeToIcon = (type: InputType): keyof typeof Icons => {
+    switch (type) {
+      case InputType.SEARCH:
+        return 'search';
+      case InputType.SELECT:
+        return 'arrowDown';
+      case InputType.DATE:
+        return 'calendar';
+      default:
+        return null;
+    }
+  };
+
+  const correspondingIconName = typeToIcon(type);
+
+  const icon: typeof Icon =
+    correspondingIconName && ((<Icon name={correspondingIconName} />) as unknown as typeof Icon);
+
+  return (
+    <FormGroup
+      label={label}
+      message={message}
+      hasError={hasError}
+      containerStyle={containerStyle}>
+      <AppTextInput
+        hasError={hasError}
+        type={type}
+        icon={icon}
+        {...restProps} />
+    </FormGroup>
+  );
+}
