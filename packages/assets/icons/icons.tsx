3d6c96b35560818825283f15e6a7d8b9b590c348
dekitsuneco <47333988+dekitsuneco@users.noreply.github.com>
2022-04-12 15:47:15 +0500

    #7 Update icons library asset

--- packages/assets/icons/icons.tsx
+++ packages/assets/icons/icons.tsx
@@ -1,6 +1,11 @@
 import { Svg } from 'ui-kit/svg';
 
 export const Icons = {
+  arrowDown: Svg(require('./chevron-down.svg')),
+  arrowLeft: Svg(require('./arrow-left.svg')),
+  calendar: Svg(require('./calendar.svg')),
+  eye: Svg(require('./eye.svg')),
+  eyeHide: Svg(require('./eye-hide.svg')),
   logoManagement: Svg(require('./logo.svg')),
-  arrowLeft: Svg(require('./arrow-left.svg'))
+  search: Svg(require('./search.svg'))
 };
