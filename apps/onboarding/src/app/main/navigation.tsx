aaf4a4ccabee531fde8e5c684be74e81945e0fc0
Vasilii <plambir93kek55@gmail.com>
2022-04-13 17:00:33 +0600

    #20: add tab menu

--- apps/onboarding/src/app/main/navigation.tsx
+++ apps/onboarding/src/app/main/navigation.tsx
@@ -3,15 +3,64 @@ import { OnboardingScreen } from './onboarding/screen';
 import { AchievementsScreen } from './achievements/screen';
 import { ProfileScreen } from './profile/screen';
 import React, { ReactElement } from 'react';
+import { createStyles } from 'ui-kit/styles';
+import { variables } from '@styles';
+import { Icon } from 'ui-kit/icon';
 
 const Tab = createBottomTabNavigator();
 
 export function MainNavigation(): ReactElement {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Onboarding' component={OnboardingScreen} />
-      <Tab.Screen name='Achievements' component={AchievementsScreen} />
-      <Tab.Screen name='ProfileScreen' component={ProfileScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: style.tabBar,
+        headerShown: false,
+        tabBarLabelStyle: [style.label],
+        tabBarActiveTintColor: variables.color.primary
+      }}>
+      <Tab.Screen
+        name='Onboarding'
+        component={OnboardingScreen}
+        options={{
+          tabBarIcon: ({ focused }) => <Icon name={focused ? 'onboardingActive' : 'onboarding'} />
+        }}
+      />
+      <Tab.Screen
+        name='Achievements'
+        component={AchievementsScreen}
+        options={{
+          tabBarIcon: ({ focused }) => <Icon name={focused ? 'achievementsActive' : 'achievements'} />
+        }}
+      />
+      <Tab.Screen
+        name='ProfileScreen'
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ focused }) => <Icon name={focused ? 'profileActive' : 'profile'} />
+        }}
+      />
     </Tab.Navigator>
   );
 }
+
+const style = createStyles({
+  tabBar: {
+    backgroundColor: variables.color.white,
+    borderRadius: 16,
+    shadowColor: variables.color.boxShadow,
+    marginHorizontal: 12,
+    marginBottom: 26,
+    height: 68,
+    paddingBottom: 7,
+    paddingTop: 14,
+    shadowOffset: {
+      width: 0,
+      height: 4
+    },
+    shadowRadius: 15,
+    elevation: 15
+  },
+  label: {
+    fontSize: 10
+  }
+});
--- packages/assets/icons/icons.tsx
+++ packages/assets/icons/icons.tsx
@@ -8,5 +8,11 @@ export const Icons = {
   scripts: Svg(require('./scripts.svg')),
   employees: Svg(require('./employees.svg')),
   logout: Svg(require('./logout.svg')),
-  logoOnboarding: Svg(require('./logo-onboarding.svg'))
+  logoOnboarding: Svg(require('./logo-onboarding.svg')),
+  onboarding: Svg(require('./onboarding.svg')),
+  profile: Svg(require('./profile.svg')),
+  achievements: Svg(require('./achievements.svg')),
+  onboardingActive: Svg(require('./onboarding-active.svg')),
+  profileActive: Svg(require('./profile-active.svg')),
+  achievementsActive: Svg(require('./achievements-active.svg'))
 };
