23393cc89b87238b08b0b35abe6cd6fe126bacb8
Vasilii <plambir93kek55@gmail.com>
2022-04-18 13:13:53 +0600

    #11: Add login form to screens

--- apps/onboarding/src/app/account-access/login/screen.tsx
+++ apps/onboarding/src/app/account-access/login/screen.tsx
@@ -1,14 +1,11 @@
 import React, { useEffect } from 'react';
 import { View, StyleSheet, ImageBackground } from 'react-native';
 import { loginFacade } from './facade';
-import { useTranslation } from 'utils/i18n';
 import { Icon } from 'ui-kit/icon';
-import { AppButton } from 'ui-kit/button';
 import { variables, commonStyle } from '@styles';
+import { LoginGroup } from 'features/login-form';
 
 export function LoginScreen(): JSX.Element {
-  const translate = useTranslation('ACCOUNT_ACCESS.LOGIN');
-
   useEffect(() => {
     loginFacade.init();
   }, []);
@@ -26,27 +23,21 @@ export function LoginScreen(): JSX.Element {
       <ImageBackground
         style={style.bgImage}
         source={require('@assets/images/background.png')}
-        resizeMode='cover'>
-        <View style={style.columnContainer}>
-          <View style={[commonStyle.flexCenter, style.column]}>
-            <View style={[commonStyle.flexCenter, style.iconContainer]}>
-              <Icon name='logoOnboarding' />
-            </View>
+        resizeMode='cover' />
+      <View style={style.columnContainer}>
+        <View style={[commonStyle.flexCenter, style.column]}>
+          <View style={[commonStyle.flexCenter, style.iconContainer]}>
+            <Icon name='logoOnboarding' />
           </View>
-          <View style={style.column}>
-            <View style={style.contentColumn}>
-              <View style={style.loginForm}>
-                <AppButton title={translate('BUTTON_SIGN_IN')} onPress={handleLoginBtn} />
-              </View>
-              <AppButton
-                title={translate('BUTTON_FORGOT_PASSWORD')}
-                theme='tertiary'
-                onPress={handleForgotPasswordBtn}
-              />
+        </View>
+        <View style={style.column}>
+          <View style={style.contentColumn}>
+            <View style={style.loginForm}>
+              <LoginGroup onSubmit={handleLoginBtn} onForgotPassword={handleForgotPasswordBtn} />
             </View>
           </View>
         </View>
-      </ImageBackground>
+      </View>
     </View>
   );
 }
@@ -56,13 +47,16 @@ const style = StyleSheet.create({
     flex: 1
   },
   bgImage: {
-    flex: 1
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '100%'
   },
   columnContainer: {
     flex: 1
   },
   column: {
-    flex: 0.5
+    height: '50%'
   },
   iconContainer: {
     backgroundColor: variables.color.background,
