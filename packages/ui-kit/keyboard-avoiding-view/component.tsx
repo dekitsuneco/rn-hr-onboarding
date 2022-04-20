87f2661c089dd9fe4d5e674e1f6674e332a27fc5
Ilya Pakhomov <ipakhomov@ronasit.com>
2022-04-21 03:18:20 +0600

    Refactored keyboard avoiding view

--- packages/ui-kit/keyboard-avoiding-view/component.tsx
+++ packages/ui-kit/keyboard-avoiding-view/component.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { KeyboardAvoidingView, KeyboardAvoidingViewProps, Platform } from 'react-native';
 
 export function AppKeyboardAvoidingView(props: KeyboardAvoidingViewProps): JSX.Element {
-  const { children, ...restProps } = props;
-
-  return (
-    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} {...restProps}>
-      {children}
-    </KeyboardAvoidingView>
-  );
+  return <KeyboardAvoidingView
+    enabled={Platform.OS === 'ios'}
+    behavior={'padding'}
+    {...props} />;
 }
