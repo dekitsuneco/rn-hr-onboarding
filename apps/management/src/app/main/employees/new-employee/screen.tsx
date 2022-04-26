577f8bbf2b375b1cde549c0a359c1d9b6579f913
Vasilii <plambir93kek55@gmail.com>
2022-04-26 17:42:05 +0600

    #22: Add from

--- apps/management/src/app/main/employees/new-employee/screen.tsx
+++ apps/management/src/app/main/employees/new-employee/screen.tsx
@@ -6,6 +6,7 @@ import { AppText } from 'ui-kit/text';
 import { useTranslation } from 'utils/i18n';
 import { EmployeeForm } from './shared/forms/employee';
 import { UploadImage } from 'ui-kit/image-upload';
+import { InputFormGroup } from 'ui-kit/input-form-group';
 
 export function NewEmployeeScreen(): ReactElement {
   const translate = useTranslation('MAIN.EMPLOYEES.NEW_EMPLOYEE');
@@ -24,6 +25,14 @@ export function NewEmployeeScreen(): ReactElement {
         <View style={style.profileDetails}>
           <AppText>{translate('SUBTITLE_PROFILE_IMAGE').toLocaleUpperCase()}</AppText>
           <UploadImage buttonText='Upload profile image' />
+          <AppText>{translate('SUBTITLE_EMPLOYEE_DETAILS').toLocaleUpperCase()}</AppText>
+          <InputFormGroup formik={formik} name='first_name' />
+          <InputFormGroup formik={formik} name='last_name' />
+          <InputFormGroup formik={formik} name='date_of_birth' />
+          <InputFormGroup formik={formik} name='email' />
+          <InputFormGroup formik={formik} name='phone_number' />
+          <InputFormGroup formik={formik} name='position' />
+          <InputFormGroup formik={formik} name='starts_on' />
         </View>
         <View style={style.roleTeamSelect} />
         <View style={style.onboarding} />
