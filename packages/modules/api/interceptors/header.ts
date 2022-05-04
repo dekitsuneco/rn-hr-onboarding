9ec8cb1de4e6ae5bfb10a965162c75fac3bc464c
Vasilii <plambir93kek55@gmail.com>
2022-05-04 20:25:55 +0600

    28: fix review bugs

--- packages/modules/api/interceptors/header.ts
+++
@@ -1,8 +0,0 @@
-import { AxiosRequestConfig } from 'axios';
-
-export const headerInterceptor =
-  (options: { ignoredEndpoints?: Array<string>; headerKey: string; headerValue: string }) => (config: AxiosRequestConfig): AxiosRequestConfig => {
-    config.headers['x-app-name'] = 'hr_onboarding';
-
-    return config;
-  };
