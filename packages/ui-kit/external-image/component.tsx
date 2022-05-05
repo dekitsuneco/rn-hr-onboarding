265157b79617a1ed0a2b4ac3b14f11ee2c773f1e
dekitsuneco <47333988+dekitsuneco@users.noreply.github.com>
2022-05-05 14:59:26 +0500

    #34 Modify ExternalImage - now it can take in headers

--- packages/ui-kit/external-image/component.tsx
+++ packages/ui-kit/external-image/component.tsx
@@ -4,12 +4,20 @@ import { Image as CachedImage } from 'react-native-expo-image-cache';
 
 interface Props {
   uri: string;
+  headers?: Record<string, string>;
   style?: StyleProp<ImageStyle>;
 }
 
-export function ExternalImage({ uri, style = {} }: Props): ReactElement {
+export function ExternalImage({ uri, headers, style = {} }: Props): ReactElement {
   const renderedImage =
-    Platform.OS === 'web' ? <Image source={{ uri }} style={style} /> : <CachedImage uri={uri} style={style} />;
+    Platform.OS === 'web' ? (
+      <Image source={{ uri, headers }} style={style} />
+    ) : (
+      <CachedImage
+        uri={uri}
+        options={headers}
+        style={style} />
+    );
 
   return renderedImage;
 }
